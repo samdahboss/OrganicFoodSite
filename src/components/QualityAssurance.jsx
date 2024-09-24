f771c38a0accae13115709eaaa0f11a93320d165
samdahboss <samuelolawumi65@gmail.com>
2024-09-24 22:54:38 +0100

    Refactor Home component to include QualityAssurance component for displaying quality features

---
+++ src/components/QualityAssurance.jsx
@@ -0,0 +1,36 @@
+import Quality from "../components/Quality";
+import { FaTruck, FaUser, FaDollarSign, FaRecycle } from "react-icons/fa";
+
+export default function QualityAssurance() {
+  const qualities = [
+    {
+      icon: <FaTruck className="text-[#8BC34A]" size={25}/>,
+      title: "Free Shipping",
+      description: "Above $5 only",
+    },
+    {
+      icon: <FaUser className="text-[#8BC34A]" size={25}/>,
+      title: "Certified Organic",
+      description: "100% Guarantee",
+    },
+    {
+      icon: <FaDollarSign className="text-[#8BC34A]" size={25}/>,
+      title: "Huge Savings",
+      description: "At Lowest Price",
+    },
+    {
+      icon: <FaRecycle className="text-[#8BC34A]" size={25}/>,
+      title: "Easy Returns",
+      description: "No Questions Asked",
+    }
+  ]
+  return (
+    <section className="w-full grid grid-cols-4 gap-4 bg-black mx-auto p-6">
+        {
+            qualities.map((quality, index) => (
+                <Quality key={index} {...quality} />
+            ))
+        }
+      </section>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import Hero from "../components/Hero";
+import QualityAssurance from "../components/QualityAssurance";
+
 export default function Home() {
   return (
     <div className=" mx-auto">
       <Hero />
+      <QualityAssurance />
     </div>
   );
 }
