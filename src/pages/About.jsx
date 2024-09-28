35751e2fd62b2e18cb0cccf7a007ca7bb611093c
samdahboss <samuelolawumi65@gmail.com>
2024-09-28 23:34:21 +0100

    Refactor About page container class to include padding

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
-import SectionTitle from '../components/SectionTitle'
-
 export default function About() {
   return (
-    <div className='container py-12 mx-auto'>
-      <SectionTitle title='About' />
+    <div className='container mx-auto'>
+      <section className="section-hero bg-[#F8F6F3] py-12">
+        <div className="container mx-auto text-center">
+          <h1 className="text-4xl font-bold font-merriweather">About Us</h1>
+        </div>
+      </section>
     </div>
   )
 }
