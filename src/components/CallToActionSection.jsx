03b178bd0beb13e7a1e7b552b09b1d76d4688e2b
samdahboss <samuelolawumi65@gmail.com>
2024-10-06 18:22:02 +0100

    -Added the Call to action section in the home page to a separate component

---
+++ src/components/CallToActionSection.jsx
@@ -0,0 +1,27 @@
+import { FaShoppingCart } from "react-icons/fa";
+export default function CallToActionSection() {
+  return (
+    <section>
+      <div className="call-to-action bg-black text-white py-6 lg:py-16">
+        <div className="container mx-auto flex flex-col lg:flex-row items-center text-center lg:text-left gap-6 justify-between lg:px-12 px-6">
+          <h1 className="lg:text-4xl text-2xl font-semibold text-white">
+            Get 25% Off On Your First Purchase!
+          </h1>
+          <button className="bg-darkGreenColor hover:bg-greenColor w-[190px] lg:w-auto text-white flex gap-2 items-center py-3 px-6 rounded-lg font-semibold">
+            <FaShoppingCart size={18} /> SHOP NOW
+          </button>
+        </div>
+      </div>
+      <div className="call-to-action bg-[#F8F6F3] texx-white  py-4">
+        <div className="container mx-auto flex justify-between lg:px-64">
+          <div className="lg:w-1/2"></div>
+          <div className="lg:w-1/2">
+            <h2 className="text-2xl lg:text-3xl font-semibold font-merriweather text-black">
+              Try It For Free. No Registration Needed.
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import BestSales from "../components/BestSales";
 import ProductHighlights from "../components/ProductHighlights";
 import TrendingProducts from "../components/TrendingProducts";
 import CustomerReviews from "../components/CustomerReviews";
-import { FaShoppingCart } from "react-icons/fa";
+import CallToActionSection from "../components/CallToActionSection";
 import FeaturedBrands from "../components/FeaturedBrands";
 export default function Home() {
   return (
@@ -13,26 +13,7 @@ export default function Home() {
       <QualityAssurance />
       <BestSales />
       <ProductHighlights />
-      <div className="call-to-action bg-black text-white py-6 lg:py-16">
-        <div className="container mx-auto flex flex-col lg:flex-row items-center text-center lg:text-left gap-6 justify-between lg:px-12 px-6">
-          <h1 className="lg:text-4xl text-2xl font-semibold text-white">
-            Get 25% Off On Your First Purchase!
-          </h1>
-          <button className="bg-darkGreenColor hover:bg-greenColor w-[190px] lg:w-auto text-white flex gap-2 items-center py-3 px-6 rounded-lg font-semibold">
-            <FaShoppingCart size={18} /> SHOP NOW
-          </button>
-        </div>
-      </div>
-      <div className="call-to-action bg-[#F8F6F3] texx-white  py-4">
-        <div className="container mx-auto flex justify-between lg:px-64">
-          <div className="lg:w-1/2"></div>
-          <div className="lg:w-1/2">
-            <h2 className="text-2xl lg:text-3xl font-semibold font-merriweather text-black">
-            Try It For Free. No Registration Needed.
-            </h2>
-          </div>
-        </div>
-      </div>
+      <CallToActionSection />
       <TrendingProducts />
       <CustomerReviews />
       <FeaturedBrands/>
