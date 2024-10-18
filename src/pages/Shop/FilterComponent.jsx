15279193907b51c8342e979465141d489e2bafba
samdahboss <samuelolawumi65@gmail.com>
2024-10-18 11:41:40 +0100

    -feature: user price input can change the range position

--- src/pages/Shop/FilterComponent.jsx
+++ src/pages/Shop/FilterComponent.jsx
@@ -1,17 +1,23 @@
-import { useContext } from "react";
+import { useContext} from "react";
 import { Range } from "react-range";
 import { FilterContext } from "./FilterProductsContext";
 
 const PriceFilter = () => {
-  const {priceRange, setPriceRange, handleFilter} = useContext(FilterContext)
-
+  const { priceRange, setPriceRange } = useContext(FilterContext);
+  const userPriceInput = (event, index) =>{
+    if(index === 0){
+      setPriceRange([parseInt(event.target.value), priceRange[1]])
+    }else if(index === 1){
+      setPriceRange([priceRange[0],parseInt(event.target.value)])
+    }
+  }
   return (
     <div className="price-filter flex flex-col gap-4">
       <h3 className="text-2xl font-bold font-merriweather">Filter by price</h3>
       <Range
         step={1}
         min={10}
-        max={41}
+        max={40}
         values={priceRange}
         onChange={(priceRange) => setPriceRange(priceRange)}
         renderTrack={({ props, children }) => (
@@ -26,7 +32,7 @@ const PriceFilter = () => {
         renderThumb={({ props, index }) => (
           <div
             {...props}
-            key={index}            
+            key={index}
             className="h-[15px] w-[15px] rounded-full bg-[#8BC34A] flex outline-0"
             style={{
               boxShadow: "0px 2px 6px #aaa",
@@ -35,13 +41,20 @@ const PriceFilter = () => {
         )}
       />
       <div className="price-priceRange flex justify-end gap-4">
-        {[priceRange[0], priceRange[1]].map((item, index) => (
+        {priceRange.map((item, index) => (
           <div
             key={index}
             className="flex w-[60px] bg-white p-2 border border-gray-300 rounded-lg p-2"
           >
             <span>£</span>
-            <input value={item} className="w-5/6 outline-0" onChange={handleFilter}/>
+            <input
+              className="w-5/6 outline-0"
+              type="number"
+              defaultValue={item}
+              max={40}
+              min={10}
+              onChange={(event)=>userPriceInput(event, index)}
+            />
           </div>
         ))}
       </div>
