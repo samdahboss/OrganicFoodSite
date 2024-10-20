fa2ca7745ea0edfd3ee76c9feae396fe27b0639d
samdahboss <samuelolawumi65@gmail.com>
2024-10-20 17:17:23 +0100

    -Created Groceries Page and Juice page from shop page

--- src/pages/Groceries/Groceries.jsx
+++ src/pages/Groceries/Groceries.jsx
@@ -1,5 +1,17 @@
+import ShopNav from "../Shop/ShopNav";
+import ShopProductDisplay from "../Shop/ShopProductDisplay";
+import SortOrderProvider from "../Shop/SortOrderContext";
+import FilterContextProvider from "../Shop/FilterProductsContext";
+
 export default function Groceries() {
   return (
-    <div>Groceries</div>
-  )
+    <section className="py-16 px-12 bg-[#F8F6F3] flex lg:flex-row flex-col-reverse">
+      <SortOrderProvider>
+        <FilterContextProvider>
+          <ShopNav />
+          <ShopProductDisplay directory="Shop/Groceries" sectionName="Groceries"/>
+        </FilterContextProvider>
+      </SortOrderProvider>
+    </section>
+  );
 }
--- src/pages/Juice/Juice.jsx
+++ src/pages/Juice/Juice.jsx
@@ -1,5 +1,18 @@
+import ShopNav from "../Shop/ShopNav";
+import ShopProductDisplay from "../Shop/ShopProductDisplay";
+import SortOrderProvider from "../Shop/SortOrderContext";
+import FilterContextProvider from "../Shop/FilterProductsContext";
+
 export default function Juice() {
   return (
-    <div>Juice</div>
-  )
+    <section className="py-16 px-12 bg-[#F8F6F3] flex lg:flex-row flex-col-reverse">
+      <SortOrderProvider>
+        <FilterContextProvider>
+          <ShopNav />
+          <ShopProductDisplay directory="Shop/Juice" sectionName="Juice"/>
+        </FilterContextProvider>
+      </SortOrderProvider>
+    </section>
+  );
 }
+
