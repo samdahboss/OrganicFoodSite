a67b383c02f8849c1943c10001beabf92c506586
samdahboss <samuelolawumi65@gmail.com>
2024-10-24 19:05:14 +0100

    -fix: related products component to display 3 different products

--- src/pages/Shop/RelatedProducts.jsx
+++ src/pages/Shop/RelatedProducts.jsx
@@ -6,24 +6,29 @@ import PropTypes from "prop-types";
 export default function RelatedProducts({ productIndex }) {
   const currentProduct = productIndex;
 
-  const generateRelatedProduct = () => {
-    let number = Math.floor(Math.random() * (allProducts.length));
-    if (number == currentProduct) {
-      generateRelatedProduct();
-    }else{
-      return number;
-    }    
+  const generateRelatedProducts = () => {
+    const relatedProductsSet = new Set();
+
+    while (relatedProductsSet.size < 3) {
+      let randomIndex = Math.floor(Math.random() * allProducts.length);
+      
+      // Ensure the randomly selected product is not the current product
+      // and has not been already selected
+      if (randomIndex !== currentProduct && !relatedProductsSet.has(randomIndex)) {
+        relatedProductsSet.add(randomIndex);
+      }
+    }
+
+    // Convert the set to an array for further use
+    return Array.from(relatedProductsSet);
   };
-  const relatedProducts = [
-    generateRelatedProduct(),
-    generateRelatedProduct(),
-    generateRelatedProduct(),
-  ];
+
+  const relatedProducts = generateRelatedProducts();
+
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid lg:grid-cols-4 gap-4">
       {relatedProducts.map((item, index) => {
         const relatedProduct = allProducts[item];
-        console.log(relatedProduct)
         return (
           <Link to={{ pathname: `/product/${item}` }} key={index}>
             <ProductCard {...relatedProduct} />
