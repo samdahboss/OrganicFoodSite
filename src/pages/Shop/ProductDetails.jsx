7481d214053aa98be88d849dafcfda94f848bb96
samdahboss <samuelolawumi65@gmail.com>
2024-10-24 15:26:03 +0100

    -created products detail landing

--- src/pages/Shop/ProductDetails.jsx
+++ src/pages/Shop/ProductDetails.jsx
@@ -1,12 +1,8 @@
-import { useParams, Link } from "react-router-dom";
-import categories from "../../utils/ShopNavCategoriesList";
-import allProducts from "../../utils/AllProducts";
+import ProductDetailsLanding from "./ProductDetailsLanding";
 import { FaRegStar } from "react-icons/fa";
 import { useState } from "react";
 
 export default function ProductDetails() {
-  const { index } = useParams();
-  const currentProduct = allProducts[parseInt(index)];
   const [reviews, setReviews] = useState(false);
   const [description, setDescription] = useState(true);
   const toggleInfo = () => {
@@ -15,62 +11,7 @@ export default function ProductDetails() {
   };
   return (
     <div className=" px-6 bg-[#F8F6F3]">
-      <div className="container lg:flex pt-16 pb-12 px-6">
-        <div className="lg:w-1/2">
-          <img src={currentProduct.image} className="w-full" />
-        </div>
-        <div className="lg:w-1/2 lg:ps-12">
-          <div className="flex flex-col gap-4">
-            <h1 className="text-[30px] font-merriweather font-bold">
-              {currentProduct.title}
-            </h1>
-            <div className="flex gap-2">
-              <h1 className="text-[24px] font-merriweather font-bold">
-                £{currentProduct.price}.00
-              </h1>
-              <span className="flex justify-center items-center text-gray-500">
-                +Free Shipping
-              </span>
-            </div>
-            <p className="text-gray-500">
-              Neque porro quisquam est, qui dolore ipsum quia dolor sit amet,
-              consectetur adipisci velit, sed quia non incidunt lores ta porro
-              ame. numquam eius modi tempora incidunt lores ta porro ame.
-            </p>
-
-            <div className="flex gap-4">
-              <input
-                type="number"
-                defaultValue={1}
-                className="w-[50px] border flex justify-center p-3 "
-              />
-              <button className="bg-darkGreenColor hover:bg-greenColor w-1/2 text-white flex justify-center gap-2 z-40 items-center py-3 px-6 rounded-lg font-semibold">
-                ADD TO CART
-              </button>
-            </div>
-
-            <hr className="bg-gray-500 h-[2px]" />
-
-            <h3 className="flex ">
-              <div className="flex items-center">Categories:</div>
-              {categories.map((item, index) => (
-                <Link
-                  key={index}
-                  to={item.link}
-                  // onClick={() => {
-                  //   setCategories(item.category);
-                  // }}
-                >
-                  <div className="ps-4 flex my-2">
-                    <h4 className="text-greenColor">{item.category}</h4>
-                    <span className="ps-2">({item.quantity})</span>
-                  </div>
-                </Link>
-              ))}
-            </h3>
-          </div>
-        </div>
-      </div>
+      <ProductDetailsLanding/>
       <div className="container px-6 border-t-[2px] flex flex-col gap-4 pb-12">
         <div>
           <ul className="flex box-border ">
