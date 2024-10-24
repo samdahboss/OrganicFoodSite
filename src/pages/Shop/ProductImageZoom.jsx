2068c2c5b02b724cb4acfa530795b88dd6a0aca7
samdahboss <samuelolawumi65@gmail.com>
2024-10-24 21:56:46 +0100

    - installed materials icon -created product Modal

--- src/pages/Shop/ProductImageZoom.jsx
+++ src/pages/Shop/ProductImageZoom.jsx
@@ -1,12 +1,26 @@
 import PropTypes from "prop-types";
+import {useState} from "react";
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
+import InnerImageZoom from "react-inner-image-zoom";
+import { FaSearch } from "react-icons/fa";
+import BasicModal from "./ProductModal";
 
 export default function ProductImageZoom({ currentProduct }) {
+  const [open, setOpen] = useState(false);
+  const toggleOpen = () => setOpen(prevOpen => !prevOpen);
   return (
     <div className="lg:w-1/2 overflow-hidden relative">
-      <img
+      <button className="absolute top-5 right-5 bg-white p-2 rounded-full z-50" onClick={toggleOpen}>
+        <FaSearch className="text-gray-600"/>
+      </button>
+      <InnerImageZoom
         src={currentProduct.lgImage}
-        className="w-full h-full object-cover transition-transform duration-300 ease-in-out transform hover:scale-150 hover:origin-center"
+        zoomScale={1.1}
+        zoomType="hover"
+        zoomPreload={true}
+        hideHint={true}
       />
+      {open && <BasicModal />}
     </div>
   );
 }
