2487c21c30987f854ccec37d1b0f1db080bab7b5
samdahboss <samuelolawumi65@gmail.com>
2024-10-26 10:47:18 +0100

    -added link to home hero btn to lead to shop

--- src/pages/Home/HomeHero.jsx
+++ src/pages/Home/HomeHero.jsx
@@ -1,35 +1,39 @@
 import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export default function HomeHero() {
   return (
     <section className="flex hero lg:py-24 relative flex-col lg:flex-row px-12 container mx-auto bg-[#FBFAF8]">
-        <div className="absolute bottom-0 right-0 w-80 hidden lg:block">
-            <img src="/leaves-free-img.png" className="z-0"/>
-            <div className="absolute bottom-0 right-0 bg-[#FBFAF8] opacity-50 z-20 w-full h-full"></div>
+      <div className="absolute bottom-0 right-0 w-80 hidden lg:block">
+        <img src="/leaves-free-img.png" className="z-0" />
+        <div className="absolute bottom-0 right-0 bg-[#FBFAF8] opacity-50 z-20 w-full h-full"></div>
+      </div>
+      <div className="hero lg:w-1/2 hidden lg:block z-20">
+        <img src="/organic-products-hero.png" alt="hero" />
+      </div>
+      <div className="lg:w-1/2 items-center text-center lg:text-left lg:items-start flex flex-col gap-4 pl-12 py-12 z-20">
+        <div>
+          <img src="/logo-leaf-new.png" alt="logo" />
         </div>
-        <div className="hero lg:w-1/2 hidden lg:block z-20">
-          <img src="/organic-products-hero.png" alt="hero" />
-        </div>
-        <div className="lg:w-1/2 items-center text-center lg:text-left lg:items-start flex flex-col gap-4 pl-12 py-12 z-20">
-          <div>
-            <img src="/logo-leaf-new.png" alt="logo" />
-          </div>
-          <h5 className="text-[17px] font-bold">Best Quality Products</h5>
-          <h1 className="lg:text-6xl text-[30px] lg:leading-[72px] font-merriweather font-bold">
-            Join The Organic Movement!
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-          </p>
-          <div className="py-4">
+        <h5 className="text-[17px] font-bold">Best Quality Products</h5>
+        <h1 className="lg:text-6xl text-[30px] lg:leading-[72px] font-merriweather font-bold">
+          Join The Organic Movement!
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+          tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+        </p>
+        <div className="py-4">
+          <Link to="/shop">
             <button className="bg-darkGreenColor hover:bg-greenColor text-white flex gap-2 z-40 items-center py-3 px-6 rounded-lg font-semibold">
-              <FaShoppingCart size={18}/> SHOP NOW
+              <FaShoppingCart size={18} /> SHOP NOW
             </button>
-          </div>
+          </Link>
         </div>
-        <div className="hero lg:w-1/2 lg:hidden z-20">
-          <img src="/organic-products-hero.png" alt="hero" className="w-full" />
-        </div>
-      </section>
-  )
+      </div>
+      <div className="hero lg:w-1/2 lg:hidden z-20">
+        <img src="/organic-products-hero.png" alt="hero" className="w-full" />
+      </div>
+    </section>
+  );
 }
